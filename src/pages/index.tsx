4f54bf0705235a9f0ac749442f53aa0c68651e49
Osagemo <osagemo@gmail.com>
2022-04-15 17:44:45 +0200

    add skeleton layout for movie lists

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,21 @@ const Home: NextPage = () => {
     return <div>Loading...</div>;
   }
 
-  return <h1 className="text-3xl font-bold underline">{data.greeting}</h1>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-center text-3xl">Movie tracker</div>
+      <div className="p-5 flex justify-between items-center w-1/2">
+        <div>
+          <h3>Watched</h3>
+          <div className="bg-gray-100 h-96 w-80"></div>
+        </div>
+        <div>
+          <h3>Wanna</h3>
+          <div className="bg-gray-100 h-96 w-80"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
